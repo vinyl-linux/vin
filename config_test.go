1d40f19269782a578901689c5ff80c1db429b3f4
jspc <james@zero-internet.org.uk>
2021-01-26 09:32:15 +0900

    Ensure non-happy path is tested

--- config_test.go
+++ config_test.go
@@ -4,6 +4,14 @@ import (
 	"testing"
 )
 
+func TestLoadConfig(t *testing.T) {
+	configFile = "this-file-should-not-exist"
+	_, err := LoadConfig()
+	if err == nil {
+		t.Errorf("expected error, received none")
+	}
+}
+
 func TestConfig_Expand(t *testing.T) {
 	configFile = "testdata/test-config.toml"
 
@@ -15,17 +23,22 @@ func TestConfig_Expand(t *testing.T) {
 	m := Manifest{}
 
 	for _, test := range []struct {
-		name   string
-		in     string
-		expect string
+		name        string
+		in          string
+		expect      string
+		expectError bool
 	}{
-		{"default configure", m.Commands.GetConfigure(), "./configure --enable-hello-world"},
-		{"default compile", m.Commands.GetCompile(), "make -j100"},
-		{"default install", m.Commands.GetInstall(), "make install -j100"},
+		{"default configure", m.Commands.GetConfigure(), "./configure --enable-hello-world", false},
+		{"default compile", m.Commands.GetCompile(), "make -j100", false},
+		{"default install", m.Commands.GetInstall(), "make install -j100", false},
+		{"dodgy template", " {{ .HelloWorld }}", "", true},
 	} {
 		t.Run(test.name, func(t *testing.T) {
 			got, err := c.Expand(test.in)
-			if err != nil {
+
+			if err == nil && test.expectError {
+				t.Error("expected error, received none")
+			} else if err != nil && !test.expectError {
 				t.Errorf("unexpected error: %+v", err)
 			}
 
