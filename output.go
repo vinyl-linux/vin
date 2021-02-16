bb4f0d0b1a5c4ec4837f20a31fb3fbbe01de8ba4
jspc <james@zero-internet.org.uk>
2021-02-16 22:21:12 +0900

    Ensure tracking operation progress ease
    Prior to this change, the output from vind (via vin) was reasonbly confusing; there was no real explanation of what line belonged to what. Come back in the middle of an install and you've no idea what package the output on the screen belongs to. This commit introduces a bright cyan-bold package at the start of every line

---
+++ output.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/fatih/color"
+	"github.com/vinyl-linux/vin/server"
+)
+
+var (
+	prefixColour = color.New(color.FgCyan, color.Bold)
+)
+
+// Outputter handles writing strings to vin
+// clients by reading strings from a chan and writing
+// them, prefixed with an additional string, to a server.OutputSender
+type Outputter struct {
+	C      chan string
+	Prefix string
+
+	o server.OutputSender
+}
+
+// NewOutputter takes an OutputSender and returns an Outputter
+// ready to be dispatched across
+func NewOutputter(o server.OutputSender) Outputter {
+	return Outputter{
+		C:      make(chan string),
+		Prefix: "",
+		o:      o,
+	}
+}
+
+// Dispatch loops over o.C, prefixes each message with o.Prefix, and writes
+// it to the internal OutputSender.
+//
+// It is generally called as a `go func`
+func (o *Outputter) Dispatch() {
+	for msg := range o.C {
+		// generate this for each message; the prefix can (and does) change often
+		prefix := prefixColour.Sprintf(o.Prefix)
+
+		o.o.Send(&server.Output{
+			Line: fmt.Sprintf("%s\t%s", prefix, msg),
+		})
+	}
+}
---
+++ output_test.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"testing"
+	"time"
+
+	"github.com/vinyl-linux/vin/server"
+)
+
+func TestNewOutputter(t *testing.T) {
+	defer func() {
+		err := recover()
+		if err != nil {
+			t.Fatalf("unexpected error: %#v", err)
+		}
+	}()
+
+	NewOutputter(&mockInstallServer{})
+}
+
+func TestOutputter_Dispatch(t *testing.T) {
+	// This is a 'Test', rather than an 'Example' in order to test
+	// escape codes
+
+	vs := &mockInstallServer{}
+	o := NewOutputter(vs)
+	go o.Dispatch()
+
+	for _, test := range []struct {
+		prefix string
+		msg    string
+		expect string
+	}{
+		{"", "Hello, world!", "\x1b[36;1m\x1b[0m\tHello, world!"},
+		{"test", "Doing Tests!", "\x1b[36;1mtest\x1b[0m\tDoing Tests!"},
+	} {
+		t.Run("", func(t *testing.T) {
+			vs.messages = []*server.Output{}
+
+			o.Prefix = test.prefix
+			o.C <- test.msg
+
+			time.Sleep(time.Millisecond * 200)
+
+			if len(vs.messages) != 1 {
+				t.Fatalf("vs.messages: expected %d, received %d", 1, len(vs.messages))
+			}
+
+			if test.expect != vs.messages[0].Line {
+				t.Errorf("expected %q, recveived %q", test.expect, vs.messages[0].Line)
+			}
+
+		})
+	}
+}
